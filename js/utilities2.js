6989e21184f6b8f730f36081d27b3d21ee6e63c6
Mahfuz Ahmed <108610927+mahfuzahmed7@users.noreply.github.com>
2025-01-16 16:24:39 +0600

    added all of this add money, cash out and transaction function

--- js/utilities2.js
+++ js/utilities2.js
@@ -11,3 +11,14 @@ function getTextFieldValueById(id) {
     return textNumber;
 
 }
+
+
+function showSectionById(id) {
+    // hide all the sections
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    // show the section with the provide id as parameter
+    document.getElementById(id).classList.remove('hidden');
+}
